db965cfd5afb3d1ac77f110e3acdb5b1c1b8e802
reyn1234 <Xyleaeron89@gmail.com>
2023-01-23 11:12:27 -0800

    My First PHP

---
+++ phpdemo/index.php
@@ -0,0 +1,22 @@
+<?php
+// echo "Hello World";
+// echo "<br>";
+// echo 123;
+
+// echo '<h1>Hello</h1>';
+
+// print 'Hello';
+
+// print_r('Hello');
+// echo "<br>";
+// print_r([1,2,3]);
+
+// $fname = 'Reyn'; 
+// $age = 37;
+
+// echo "$fname is $age years older";
+
+// var_dump('Hello');
+var_dump([1,2,3]);
+
+?>
